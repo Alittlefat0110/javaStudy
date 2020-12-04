0ec3c4d1b6bf36e0f51b21445bffa911089752b4
ChubbyFace <522009006@qq.com>
2020-12-04 18:19:26 +0800

    12.4 更新（lambda表达式）

---
+++ src/main/java/com/javaBasic/study/intermediate/lambda/lambdaAggregate.java
@@ -0,0 +1,4 @@
+package com.javaBasic.study.intermediate.lambda;
+
+public class lambdaAggregate {
+}
---
+++ src/main/java/com/javaBasic/study/intermediate/lambda/lambdaMethodQuote.java
@@ -0,0 +1,4 @@
+package com.javaBasic.study.intermediate.lambda;
+
+public class lambdaMethodQuote {
+}
